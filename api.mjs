18d4720748f75190fafd9d3a41c82ffdecd47b42
Comeprimas <134971962+Comeprimas@users.noreply.github.com>
2024-07-19 16:53:19 -0300

    Update api.mjs

--- api.mjs
+++ api.mjs
@@ -1,122 +1,97 @@
 import express from 'express';
-import ytdl from 'youtubedl-core';
-import { searchMusics } from 'node-youtube-music'; // Tentando uma importação específica
-import ffmpeg from 'fluent-ffmpeg';
-import NodeID3 from 'node-id3';
 import fs from 'fs';
 import { randomBytes } from 'crypto';
+import ytdl from 'ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
+import yts from 'yt-search';
 
 const app = express();
-const port = 3000;
-
-const ytIdRegex = /(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/;
+const port = process.env.PORT || 1000;
 
 class YT {
-    static isYTUrl = (url) => ytIdRegex.test(url);
-
-    static getVideoID = (url) => {
-        if (!this.isYTUrl(url)) throw new Error('is not YouTube URL');
-        return ytIdRegex.exec(url)[1];
-    };
-
-    static WriteTags = async (filePath, Metadata) => {
-        NodeID3.write(
-            {
-                title: Metadata.Title,
-                artist: Metadata.Artist,
-                originalArtist: Metadata.Artist,
-                image: {
-                    mime: 'jpeg',
-                    type: {
-                        id: 3,
-                        name: 'front cover',
-                    },
-                    imageBuffer: (await fetchBuffer(Metadata.Image)).buffer,
-                    description: `Cover of ${Metadata.Title}`,
-                },
-                album: Metadata.Album,
-                year: Metadata.Year || ''
-            },
-            filePath
-        );
-    };
-
-    static searchTrack = async (query) => {
+    static async search(query) {
         try {
-            let ytMusic = await searchMusics(query); // Usando a importação específica
-            return ytMusic.map(track => ({
-                isYtMusic: true,
-                title: `${track.title} - ${track.artists.map(x => x.name).join(' ')}`,
-                artist: track.artists.map(x => x.name).join(' '),
-                id: track.youtubeId,
-                url: 'https://youtu.be/' + track.youtubeId,
-                album: track.album,
-                duration: {
-                    seconds: track.duration.totalSeconds,
-                    label: track.duration.label
-                },
-                image: track.thumbnailUrl.replace('w120-h120', 'w600-h600')
-            }));
+            const searchResults = await yts(query);
+            return searchResults.videos;
         } catch (error) {
-            throw error;
+            throw new Error('Erro ao pesquisar vídeos no YouTube');
         }
-    };
+    }
 
-    static downloadMusic = async (query) => {
+    static async downloadMusic(url) {
         try {
-            const getTrack = await this.searchTrack(query);
-            const search = getTrack[0];
-            const videoInfo = await ytdl.getInfo('https://www.youtube.com/watch?v=' + search.id, { lang: 'id' });
-            let stream = ytdl(search.id, { filter: 'audioonly', quality: 140 });
-            let songPath = `./XliconMedia/audio/${randomBytes(3).toString('hex')}.mp3`;
-            stream.on('error', (err) => console.log(err));
-
-            const file = await new Promise((resolve) => {
+            const stream = ytdl(url, { filter: 'audioonly' });
+            const songPath = `./audio/${randomBytes(3).toString('hex')}.mp3`;
+            await new Promise((resolve, reject) => {
                 ffmpeg(stream)
-                    .audioFrequency(44100)
-                    .audioChannels(2)
                     .audioBitrate(128)
-                    .audioCodec('libmp3lame')
-                    .audioQuality(5)
-                    .toFormat('mp3')
                     .save(songPath)
-                    .on('end', () => resolve(songPath));
+                    .on('end', () => resolve(songPath))
+                    .on('error', reject);
             });
-
-            await this.WriteTags(file, {
-                Title: search.title,
-                Artist: search.artist,
-                Image: search.image,
-                Album: search.album,
-                Year: videoInfo.videoDetails.publishDate.split('-')[0]
-            });
-
-            return {
-                meta: search,
-                path: file,
-                size: fs.statSync(songPath).size
-            };
+            return songPath;
         } catch (error) {
-            throw new Error(error);
+            throw new Error('Erro ao baixar música');
         }
-    };
+    }
 }
 
-app.get('/api/mp3', async (req, res) => {
-    const { name } = req.query;
-    if (!name) {
-        return res.status(400).json({ error: 'Name parameter is required' });
-    }
+app.get('/api/download/mp3', async (req, res) => {
+    let { url, name } = req.query;
     try {
-        const result = await YT.downloadMusic(name);
-        res.json(result);
+        if (!url && !name) {
+            throw new Error('Parâmetro URL ou nome é obrigatório');
+        }
+
+        url = url ? encodeURI(url) : undefined;
+        name = name ? encodeURI(name) : undefined;
+
+        let downloadUrl = url;
+        let videoInfo = {}; // Objeto para armazenar informações do vídeo
+
+        if (name) {
+            const searchResults = await YT.search(name);
+            if (searchResults.length === 0) {
+                throw new Error('Nenhum resultado encontrado para o nome fornecido');
+            }
+            downloadUrl = `https://www.youtube.com/watch?v=${searchResults[0].videoId}`;
+            videoInfo = searchResults[0]; // Armazenar informações do primeiro resultado
+        } else {
+            const videoDetails = await ytdl.getInfo(url);
+            videoInfo = {
+                title: videoDetails.videoDetails.title,
+                thumbnail: videoDetails.videoDetails.thumbnail.thumbnails[0].url, // URL da capa do vídeo
+                views: videoDetails.videoDetails.viewCount, // Número de visualizações do vídeo
+                uploadDate: videoDetails.videoDetails.uploadDate, // Data de upload do vídeo
+                lengthSeconds: videoDetails.videoDetails.lengthSeconds // Duração do vídeo em segundos
+                // Você pode adicionar mais informações aqui, se necessário
+            };
+        }
+
+        const songPath = await YT.downloadMusic(downloadUrl);
+        const fileName = songPath.split('/').pop();
+        const downloadUrl = `http://${req.headers.host}/audio/${fileName}`;
+
+        // Adicionar informações do vídeo à resposta JSON
+        res.json({ downloadUrl, videoInfo });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
-app.listen(port, () => {
-    console.log(`API is running on http://localhost:${port}`);
+app.get('/audio/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+    const filePath = `./audio/${fileName}`;
+    res.download(filePath, 'download.mp3', (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Falha ao baixar o arquivo' });
+        } else {
+            fs.unlinkSync(filePath); // Excluir o arquivo após o download
+        }
+    });
 });
 
-export default app;
+app.listen(port, () => {
+    console.log(`Servidor está rodando na porta ${port}`);
+});
